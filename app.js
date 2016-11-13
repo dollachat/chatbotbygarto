f1564710c93a5d3d678824d6e8740918715ab5ef
dollachat <dollachat@gmail.com>
2016-11-14 01:28:59 +0700

    ของเก่า

--- app.js
+++ app.js
@@ -816,8 +816,7 @@ function sendAccountLinking(recipientId) {
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  function sendglaon(recipientId) {
-
+function sendglaon(recipientId) {
   var messageData = {
     recipient: {
       id: recipientId
@@ -828,13 +827,41 @@ function sendAccountLinking(recipientId) {
       
     
   };  
-  
+
   callSendAPI(messageData);
-sendonled(recipentId);
+  sendonled();
 }
 
 function sendglaoff(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+       text:"LED OFF"
+        }
+      
+    
+  };  
+  
+  
+  function sendglaon(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+       text:"LED ON"
+        }
+      
+    
+  };  
 
+  callSendAPI(messageData);
+  sendonled();
+}
+
+function sendglaoff(recipientId) {
   var messageData = {
     recipient: {
       id: recipientId
@@ -847,10 +874,14 @@ function sendglaoff(recipientId) {
   };  
 
   callSendAPI(messageData);
-  sendoffled(recipentId);
+  sendoffled();
 }
 
- 
+
+
+  callSendAPI(messageData);
+  sendoffled();
+}
 
 function sendcheckid(recipientId) {
   var messageData = {
@@ -858,8 +889,8 @@ function sendcheckid(recipientId) {
       id: recipientId
     },
     message: {
-       text:"RecipentID is " + recipientId 
-         }
+       text:"Your ID is " + recipientId
+        }
       
     
   };  
@@ -873,8 +904,7 @@ function sendcheckid(recipientId) {
  
 
 
-function sendonled(recipentId){
-var RID=recipentId;
+function sendonled(){
 
 var connection = mysql.createConnection({
 host     : 'nt71li6axbkq1q6a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
@@ -885,20 +915,16 @@ host     : 'nt71li6axbkq1q6a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
 
 connection.connect();
 
-//var sql = "UPDATE raspberrypi SET state = '1' WHERE raspberrypi = 'RPI1' ";
+var sql = "UPDATE raspberrypi SET state = '1' WHERE raspberrypi = 'RPI1' ";
 
-var sql = "UPDATE raspberrypi  INNER JOIN recipentpi ON raspberrypi.raspberrypi=recipentpi.raspberrypi  \
- SET state = '1'    WHERE recipentpi.recipentid =?";
-
- connection.query(sql,RID, function(err) {
+connection.query(sql, function(err) {
     if (err) throw err;
     connection.end();
 });
 
 }
 
-function sendoffled(recipentId){
-var RID=recipentId;
+function sendoffled(){
 var connection = mysql.createConnection({
 host     : 'nt71li6axbkq1q6a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   user     : 'c4xt0mnh7gsp6lee',
@@ -908,16 +934,12 @@ host     : 'nt71li6axbkq1q6a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
 
 connection.connect();
 
-//var sql = "UPDATE raspberrypi SET state = '0' WHERE raspberrypi = 'RPI1' ";
-var sql = "UPDATE raspberrypi  INNER JOIN recipentpi ON raspberrypi.raspberrypi=recipentpi.raspberrypi  \
- SET state = '0'    WHERE recipentpi.recipentid =?";
+var sql = "UPDATE raspberrypi SET state = '0' WHERE raspberrypi = 'RPI1' ";
 
-
-connection.query(sql,RID, function(err) {
+connection.query(sql, function(err) {
     if (err) throw err;
-   connection.end();
+    connection.end();
 });
-
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
